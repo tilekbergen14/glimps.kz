0ee3efcd68f753eaeeaaf23466a0d6de76a9bded
Tilekergen Mukhamet <tilekbergen.third@gmail.com>
2022-02-26 19:26:51 +0600

    initial commit

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Woobin | Online streaming site!</title>
+    <link rel="stylesheet" href="./assets/bootstrap/css/bootstrap.min.css">
+    <script src="./assets/bootstrap/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="./assets/style.css">
+</head>
+
+<body>
+    <div class="header">
+        <img class="header-img" src="./assets/images/movie.jpg" alt="">
+        <nav class="navbar navbar-expand-lg navbar-dark">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Woobin</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse " id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0 text-light">
+                    </ul>
+                    <form class="d-flex">
+                        <button class="btn btn-primary" type="submit">SIGN IN</button>
+                    </form>
+                </div>
+            </div>
+        </nav>
+        <div class='welcome'>
+            <div class="w-75 text-center">
+                <h1 class="welcome-text">Welcome to Woobin!</h1>
+                <p class="welcome-p">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, rem illo perspiciatis sint eligendi, nisi id vero in impedit nobis quo mollitia. Facere perferendis incidunt ut, fugit molestias itaque obcaecati ab et quidem voluptas optio asperiores consequatur ullam quae deleniti recusandae! Explicabo accusamus illo eveniet placeat temporibus doloremque dicta nihil!</p>
+            </div>
+        </div>
+    </div>
+    <div class="hero">
+        <p class="title">Latest Shows</p>
+    </div>
+</body>
+
+</html>
